8837c71afe20f83e891dd8f91d9931ef46b6c040
Bezael <bezael_e@hotmail.com>
2020-06-07 17:13:52 +0200

    Add services & Class FileItem

--- src/app/shared/upload-image/models/file-item.ts
+++ src/app/shared/upload-image/models/file-item.ts
@@ -1,1 +1,11 @@
-export class FileItem {}
+import { Observable } from 'rxjs';
+export class FileItem {
+  public name: string;
+  public uploading = false;
+  public uploadPercent: Observable<number>;
+  public downloadURL: Observable<string>;
+
+  constructor(public file: File = file) {
+    this.name = file.name;
+  }
+}
---
+++ src/app/shared/upload-image/services/storage.service.ts
@@ -0,0 +1,38 @@
+import { Injectable } from '@angular/core';
+
+import { AngularFireStorage } from '@angular/fire/storage/storage';
+import { finalize } from 'rxjs/operators';
+
+import { FileItem } from '@shared/upload-image/models/file-item';
+
+@Injectable()
+export class StorageService {
+  private MEDIA_STORAGE_PATH = 'dominicode';
+
+  constructor(private readonly storage: AngularFireStorage) {}
+
+  uploadImage(images: FileItem[]) {
+    for (const item of images) {
+      item.uploading = true;
+
+      const filePath = this.generateFileName(item.name);
+      const fileRef = this.storage.ref(filePath);
+      const task = this.storage.upload(filePath, item.file);
+
+      item.uploadPercent = task.percentageChanges();
+      task
+        .snapshotChanges()
+        .pipe(
+          finalize(() => {
+            item.downloadURL = fileRef.getDownloadURL();
+            item.uploading = false;
+          })
+        )
+        .subscribe();
+    }
+  }
+
+  private generateFileName(name: string): string {
+    return `${this.MEDIA_STORAGE_PATH}/${new Date().getTime()}_${name}`;
+  }
+}
